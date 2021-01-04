a95a393705455fabb299ae7e887daf2c0b136885
frank9th <ap.teamservices@gmail.com>
2021-01-04 16:20:16 +0100

    added recent changes

--- Shop/views.py
+++ Shop/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def home(request):
+	return render(request, 'home.html',{"title":home})
+
+def login(request):
+	return render(request, 'login.html',{"title":login})
+
+def register(request):
+	return render(request, 'register.html',{"title":register})
+
+
+def dashboard(request):
+	return render(request, 'dashboard.html',{"title":dashboard})
+
+
