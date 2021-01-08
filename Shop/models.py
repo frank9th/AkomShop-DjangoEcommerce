4847899f53cd5f2fda4eec93f8ad98d758e602a1
frank9th <ap.teamservices@gmail.com>
2021-01-08 13:52:57 +0100

    querring product to cart and checkout page

--- Shop/models.py
+++ Shop/models.py
@@ -23,6 +23,16 @@ class Product(models.Model):
 	def __str__(self):
 		return self.name
 
+
+	@property
+	def imageURL(self):
+		try:
+			url = self.image.url
+		except:
+			url = ''
+		return url
+	
+
 class Order(models.Model):
 	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
 	date_ordered = models.DateTimeField(auto_now_add=True)
@@ -32,6 +42,19 @@ class Order(models.Model):
 
 	def __str__(self):
 		return str(self.id)
+	# total price of items in cart 
+	@property
+	def get_cart_total(self):
+		orderitems = self.orderitem_set.all()
+		total = sum([item.get_total for item in orderitems])
+		return total
+
+	# total quantity of itmes in cart 
+	@property
+	def get_cart_items(self):
+		orderitems = self.orderitem_set.all()
+		total = sum([item.quantity for item in orderitems])
+		return total
 
 
 
@@ -40,6 +63,14 @@ class OrderItem(models.Model):
 	order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
 	quantity = models.IntegerField(default=0, null=True, blank=True)
 	date_added = models.DateTimeField(auto_now_add=True)
+
+#fuction for calculating the totoal of product - this is a propert of orderItems
+	@property
+	def get_total(self):
+		total = self.product.price * self.quantity
+
+		return total
+	
 
 class ShippingAddress(models.Model):
 	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
--- Shop/urls.py
+++ Shop/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('store/', views.store, name='store'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
     path('login', views.loginPage, name='login'),
     path('logout', views.logoutUser, name='logout'),
     path('register', views.register, name='register'),
