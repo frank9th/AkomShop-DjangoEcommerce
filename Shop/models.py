eefd3625c1c684490e6c31a16dc5117447cb4f60
frank9th <ap.teamservices@gmail.com>
2021-01-12 19:41:19 +0100

    fixed bug

--- Shop/models.py
+++ Shop/models.py
@@ -11,7 +11,7 @@ class Customer(models.Model):
 	phone = models.CharField(max_length=200, null=True)
 
 	def __str__(self):
-		return self.name
+		return self.name 
 
 
 class Product(models.Model):
@@ -22,7 +22,7 @@ class Product(models.Model):
 	image = models.ImageField(null=True, blank=True)
 
 	def __str__(self):
-		return self.name
+		return self.name + "|"+ str(self.digital)
 
 
 	@property
@@ -42,7 +42,7 @@ class Order(models.Model):
 
 
 	def __str__(self):
-		return str(self.id)
+		return str(self.complete) + " | " + str(self.id) 
 
 	# logic to display shipping form if physical product is in cart
 	@property
@@ -77,6 +77,10 @@ class OrderItem(models.Model):
 	quantity = models.IntegerField(default=0, null=True, blank=True)
 	date_added = models.DateTimeField(auto_now_add=True)
 
+	def __str__(self):
+		return self.product.name + " | " + str(self.date_added)
+
+
 #fuction for calculating the totoal of product - this is a propert of orderItems
 	@property
 	def get_total(self):
@@ -96,4 +100,8 @@ class ShippingAddress(models.Model):
 	zipcode = models.IntegerField(default=0, null=True, blank=True)
 
 	def __str__(self):
-		return self.address
+		return self.address + " |  " + self.customer.name + " | " + self.order.transaction_id
+
+
+class BillingAddress(models.Model):
+	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
